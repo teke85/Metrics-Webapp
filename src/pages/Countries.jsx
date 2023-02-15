2f8730f0172346b03e155fac0befa04f23dea0d7
teke <mutau85@gmail.com>
2023-02-15 19:10:33 +0200

    react setup, fetch api, configure redux

---
+++ src/pages/Countries.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+import { getCountries } from '../redux/country/countriesSlice';
+import Country from '../components/Country';
+import Wrapper from '../components/Wrapper';
+
+const Countries = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const continent = JSON.parse(searchParams.get('continent') || '{}');
+
+  const { region, map, population, name, noOfCountries } = continent;
+  const { countries, status, error } = useSelector((state) => state.countries);
+
+  useEffect(() => {
+    if (
+      countries.length === 0 ||
+      region !== countries[0].region ||
+      name !== countries[0].continent
+    ) {
+      dispatch(getCountries({ region, name }));
+    }
+  }, [countries, dispatch]);
+
+  const handleCountryClick = (country) => {
+    navigate(`/country?country=${JSON.stringify(country)}`);
+  };
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <div className="main-container">
+      <Wrapper
+        className="heading-color"
+        map={map}
+        name={name}
+        population={population}
+      />
+      <div className="title-sec">
+        <h4 className="heading-color">
+          {noOfCountries} Countries in {name}
+        </h4>
+      </div>
+      <Navbar>
+        <Nav className="navbar">
+          {countries.map((country) => (
+            <Nav.Link
+              className="nav-item"
+              key={country.id}
+              to={country.path}
+              onClick={() => {
+                handleCountryClick(country);
+              }}
+            >
+              <Country country={country} />
+            </Nav.Link>
+          ))}
+        </Nav>
+      </Navbar>
+    </div>
+  );
+};
+export default Countries;
---
+++ src/pages/CountryDetails.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Wrapper from '../components/Wrapper';
+import './countryDetails.css';
+
+const CountryDetails = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const country = JSON.parse(searchParams.get('country') || '{}');
+
+  const { name, population, area, official, capital, lat, lng, flag } = country;
+
+  return (
+    <div className="main-container">
+      <Wrapper
+        className="heading-color"
+        map={country.map}
+        name={country.name}
+        population={country.population}
+      />
+      <div className="section-title">
+        <h4 className="heading-color">{name}</h4>
+        <img src={flag} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="item">
+          <span className="heading-color">Official Name</span>
+          <span className="heading-color">{official}</span>
+        </div>
+        <div className="item heading-color">
+          Capital
+          <span>{capital}</span>
+        </div>
+        <div className="item heading-color">
+          Population
+          <span>{population}</span>
+        </div>
+        <div className="item heading-color">
+          Area
+          <span>{area} sq. km</span>
+        </div>
+        <div className="item heading-color">
+          Latitude
+          <span>{lat}</span>
+          Longitude
+          <span>{lng}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountryDetails;
